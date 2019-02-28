7edcdf5c983c51e2734ce19d5cd8cf954f0ecbfb
Querijn Heijmans <querijn@irule.at>
2019-02-28 12:11:12 +0100

    Remove existing dist files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_app_1 = require("./server-app");
-const server = new server_app_1.default();
-server.get('/', function (req, res) {
-    res.send(':)');
-});
-server.run();
-//# sourceMappingURL=index.js.map
