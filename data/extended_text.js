2324f4fd72c24ad4a76587aa23a8338b86741944
Querijn Heijmans <querijn@irule.at>
2019-03-01 14:29:43 +0100

    Add weather to frontend

---
+++ data/extended_text.js
@@ -0,0 +1,122 @@
+var Querijn = Querijn || {};
+
+Querijn.Text = class extends PIXI.Text {
+
+    constructor (text, textStyle, canvas) {
+        super(text, textStyle, canvas);
+        
+        this.sprites = {};
+        this.usage = {};
+    }
+
+    updateText (respectDirty) {
+
+        if (this.dirty) {
+
+            while (this.children.length) {
+                this.removeChild(this.children[0]);
+            }
+
+            this.usage = {};
+            var characters = String.prototype.split.call(this.text, '');
+            var index = 0;
+            while (index < this.text.length) {
+                var current = characters[index++];
+                var charCode = current.charCodeAt(0);
+                var specialCharacterIndex = charCode - Querijn.Text.IdentifierBase;
+                if (specialCharacterIndex < 0 || specialCharacterIndex >= 255)
+                    continue;
+            }
+        }
+
+        super.updateText(respectDirty);
+    }
+
+    drawLetterSpacing(text, x, y, isStroke = false) {
+        
+        var isStroke = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
+
+        var style = this._style;
+
+        // letterSpacing of 0 means normal
+        var letterSpacing = style.letterSpacing;
+
+        var characters = String.prototype.split.call(text, '');
+        var currentPosition = x;
+        var index = 0;
+        var current = '';
+        var currentWidth = 0;
+
+        while (index < text.length) {
+            current = characters[index++];
+            currentWidth = this.context.measureText(current).width;
+
+            var charCode = current.charCodeAt(0);
+            var specialCharacterIndex = charCode - Querijn.Text.IdentifierBase;
+            if (specialCharacterIndex >= 0 && specialCharacterIndex < 255) {
+
+                if (!this.usage[specialCharacterIndex]) {
+                    this.usage[specialCharacterIndex] = 0;
+                }
+
+                var spriteIndex = this.usage[specialCharacterIndex]++;
+                var img = this.makeSprite(specialCharacterIndex);
+                img.visible = true;
+
+                var imgRescale = currentWidth / img.width;
+
+                img.width = currentWidth;
+                img.height = img.height * imgRescale;
+                img.x = currentPosition;
+                img.y = y;
+            }
+            else if (isStroke) {
+                this.context.strokeText(current, currentPosition, y);
+            } else {
+                this.context.fillText(current, currentPosition, y);
+            }
+            currentPosition += currentWidth + letterSpacing;
+        }
+    }
+
+    makeSprite(charIndex) {
+
+        var img = new PIXI.Sprite(Querijn.Text.identifierBinding[charIndex]);
+
+        // Due to the fact the updating of the widths happens inside the draw function, it won't happen until the next frame
+        // This means I have to show the image before that happens which means i cannot make .visible = false, so I put it off the screen
+        img.x = -9999;
+        img.y = -9999;
+        
+        var characterWidth = this.context.measureText(Querijn.Text.identifierBinding[charIndex].character).width;
+        var imgRescale = characterWidth / img.width;
+
+        img.anchor.y = 0.9;
+        img.width = characterWidth;
+        img.height = img.height * imgRescale;
+        
+        this.addChild(img);
+        
+        if (!this.sprites[charIndex])
+            this.sprites[charIndex] = [];
+
+        this.sprites[charIndex].push(img);
+        return img;
+    }
+}
+
+Querijn.Text.identifierBinding = [];
+Querijn.Text.addImage = function(texture) {
+    
+    // calculate character
+    var i = Querijn.Text.identifierBinding.length;
+    var character = String.fromCharCode(Querijn.Text.IdentifierBase + i);
+
+    // Add to our collection
+    Querijn.Text.identifierBinding.push(texture);
+    texture.character = character;
+
+    return character;
+}
+
+Querijn.Text.IdentifierBase = 56192;
---
+++ data/weather.js
@@ -0,0 +1,86 @@
+function requestWeather(onReceived) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			onReceived(JSON.parse(xhttp.responseText));
+		}
+	};
+	xhttp.open("GET", "weather.json", true);
+	xhttp.send();
+}
+
+const loader = new PIXI.loaders.Loader();
+let weatherLine = new Querijn.Text("", {
+	fontFamily: 'Arial', 
+	fontSize: 18,
+	fill: 0xffffff, 
+	align: 'left',
+	dropShadow: true,
+	dropShadowAngle: 0,
+	dropShadowBlur: 10,
+	dropShadowColor: "black",
+	dropShadowDistance: 0
+});
+weatherLine.x = weatherLine.y = 10;
+
+let weatherIcons = {
+	zonnig: null,
+	bliksem: null,
+	regen: null,
+	buien: null,
+	hagel: null,
+	mist: null,
+	sneeuw: null,
+	bewolkt: null,
+	halfbewolkt: null,
+	zwaarbewolkt: null,
+	nachtmist: null,
+	helderenacht: null,
+	wolkennacht: null,
+};
+
+for (let iconName in weatherIcons) 
+	loader.add(iconName, 'images/icons/' + iconName + '.png');
+loader.load(setupWeatherIcons);
+
+function setupWeatherIcons(loader, resources) {
+	for (let iconName in weatherIcons) {
+		weatherIcons[iconName] = Querijn.Text.addImage(resources[iconName].texture);
+		console.log("Loaded weather icon for " + iconName);
+	}
+
+	updateWeather();
+}
+
+var tickerAdded = false;
+function weatherFrame(time) {
+	app.stage.addChild(weatherLine);
+}
+
+function updateWeather() {
+	requestWeather(function (data) {
+
+		let text = "";
+		let y = 0;
+		for (let i = 0; i < data.liveweer.length; i++) {
+
+			if (i !== 0) text += "\n";
+			const weather = data.liveweer[i];
+
+			text += weather.plaats + ":\n";
+			text += "nu " + weather.temp + " °C, voelt als " + weather.gtemp + " °C\n";
+			text += "vandaag " + weatherIcons[weather.d0weer] + " " + weather.d0tmin + "~" + weather.d0tmax + " °C\n";
+			text += "morgen " + weatherIcons[weather.d1weer] + " " + weather.d1tmin + "~" + weather.d1tmax + " °C\n";
+		}
+
+		// Somehow when you set text to "", it becomes " ", so I have to put it in another var.
+		weatherLine.text = text;
+		
+		if (!tickerAdded) {
+			app.ticker.add(weatherFrame);
+			tickerAdded = true;
+		}
+	});
+
+	setTimeout(updateWeather, 1000 * 60 * 20);
+};
